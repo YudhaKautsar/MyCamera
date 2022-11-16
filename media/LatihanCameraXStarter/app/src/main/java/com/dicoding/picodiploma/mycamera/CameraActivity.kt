92c91815eb6bc32c7b3fff4d604cda584c6a72a6
yudhakautsar <muhammad.kautsar@iconpln.co.id>
2022-11-16 14:17:06 +0700

    setup camera x

--- media/LatihanCameraXStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
+++ media/LatihanCameraXStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
@@ -1,14 +1,25 @@
 package com.dicoding.picodiploma.mycamera
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.mycamera.databinding.ActivityCameraBinding
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
+
+    private lateinit var imageCapture : ImageCapture
+    private var cameraSelector : CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +28,12 @@ class CameraActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.captureImage.setOnClickListener { takePhoto() }
-        binding.switchCamera.setOnClickListener { startCamera() }
+        binding.switchCamera.setOnClickListener {
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
+            startCamera() }
     }
+
 
     public override fun onResume() {
         super.onResume()
@@ -27,11 +42,60 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
-       // takePhoto
+       val imageCapture = imageCapture
+
+        val photoFile = createFile(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra("picture", photoFile)
+                    intent.putExtra(
+                        "isBackCamera",
+                        cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+                    )
+                    setResult(MainActivity.CAMERA_X_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity, "Gagal mengambil gambar.", Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+
+        )
     }
 
     private fun startCamera() {
-        // showCamera
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            }catch (e: Exception) {
+                Toast.makeText(this, "Gagal Memunculkan Camera.", Toast.LENGTH_SHORT).show()
+            }
+        },ContextCompat.getMainExecutor(this))
     }
 
     private fun hideSystemUI() {
