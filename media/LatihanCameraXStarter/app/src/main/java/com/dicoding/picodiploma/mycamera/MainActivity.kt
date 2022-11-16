351b3084cdf4ddf78cf86b3a6b8d5d459b80bd14
yudhakautsar <muhammad.kautsar@iconpln.co.id>
2022-11-16 16:05:44 +0700

    setup intent camera

--- media/LatihanCameraXStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ media/LatihanCameraXStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -4,12 +4,15 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
 import java.io.File
 
@@ -69,7 +72,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startTakePhoto() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this,
+                "com.dicoding.picodiploma.mycamera",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
 
     private fun startCameraX() {
@@ -86,6 +101,17 @@ class MainActivity : AppCompatActivity() {
 
             val result = rotateBitmap(BitmapFactory.decodeFile(myFile.path), isBackCamera)
 
+            binding.previewImageView.setImageBitmap(result)
+        }
+    }
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            val result = BitmapFactory.decodeFile(myFile.path)
             binding.previewImageView.setImageBitmap(result)
         }
     }
